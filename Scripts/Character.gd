eec7c2961fd7ed8533c5eae75915098fb9643c1a
WaterWhirl <leonardo-zende@hotmail.com>
2022-09-03 19:34:46 -0300

    Third Person Cam

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -7,25 +7,37 @@ var jump_force := 6
 var gravity := Vector3.DOWN * 12
 var mouse_rotation := 0.05
 
+var is_jumping := false
+
 func _physics_process(delta):
 	velocity += gravity * delta
+	
+	if is_jumping:
+		velocity.y += jump_force
+	
 	move_input()
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 	
 func move_input():
-	velocity.z = 0
-	velocity.x = 0
+	var velocity_y = velocity.y
+	
+	velocity = Vector3.ZERO
 	
 	if Input.is_action_pressed("move_forward"):
-		velocity.z -= speed
+		velocity -= transform.basis.z * speed
 	if Input.is_action_pressed("move_backward"):
-		velocity.z += speed
+		velocity += transform.basis.z * speed
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= speed
+		velocity -= transform.basis.x * speed
 	if Input.is_action_pressed("move_right"):
-		velocity.x += speed
-	if Input.is_action_just_pressed("jump"):
-		velocity.y += jump_force
+		velocity += transform.basis.x * speed
+	
+	velocity.y = velocity_y
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		is_jumping = true
+	else:
+		is_jumping = false
 		
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
